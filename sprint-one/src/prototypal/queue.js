5f421d40567c830f258267e85af3456b10c11441
Jose Velez <josevelezq7@gmail.com>
2018-11-07 15:30:38 -0800

    Implement Stack and Queue using the Prototypal Inheritance Pattern

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let someInstance = Object.create(queueMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue: function(value) {
+    if (this.size() === 0) {
+      this.storage.size = 0;
+      this.storage.head = 0;
+    }
+
+    this.storage.size++;
+    this.storage[this.size() + this.storage.head - 1] = value;
+  },
+
+  dequeue: function() {
+    if (this.size() === 0) {
+      return;
+    }
+
+    let val = this.storage[this.storage.head];
+    this.storage[this.storage.head] = undefined;
+    this.storage.head++;
+    this.storage.size--;
+    return val;
+  },
+
+  size: function() {
+    if (this.storage.size === undefined) {
+      this.storage.size = 0;
+    }
+    return this.storage.size;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  let someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  push: function(value) {
+    this.storage[this.size()] = value;
+    this.storage.size++;
+  }, 
+
+  pop: function() {
+    if (this.size() === 0) {
+      return;
+    }
+
+    let val = this.storage[this.size() - 1];
+    this.storage[this.size() - 1] = undefined;
+    this.storage.size--;
+    return val;
+
+  }, 
+
+  size: function() {
+    if (this.storage.size === undefined) {
+      this.storage.size = 0;
+    }
+
+    return this.storage.size;
+  }
+};
 
 
