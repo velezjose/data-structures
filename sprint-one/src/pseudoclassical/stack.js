51196b1266611b7ba8c715d8e41edf2a1382b3e6
Jose Velez <josevelezq7@gmail.com>
2018-11-07 16:02:17 -0800

    Implement Stack and Queue using Pseudoclassical Pattern

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,28 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[this.size()] = value;
+  this.storage.size++;
+}
 
+Stack.prototype.pop = function() {
+  if (this.size() === 0) {
+    return;
+  }
+
+  let val = this.storage[this.size() - 1];
+  this.storage[this.size() - 1] = undefined;
+  this.storage.size--;
+  return val;
+}
+
+Stack.prototype.size = function() {
+  if (this.storage.size === undefined) {
+    this.storage.size = 0;
+  }
+  return this.storage.size;
+}
