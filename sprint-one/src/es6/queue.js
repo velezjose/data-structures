08423d58a3c59b0b8aa1de3e3a4e117dba0521d1
Jose Velez <josevelezq7@gmail.com>
2018-11-07 16:11:06 -0800

    Implement Queue and Stack using ES6 class pattern

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,39 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.storage.size = 0;
   }
+
+  enqueue(value) {
+    if (this.size() === 0) {
+      this.storage.head = 0;
+    }
+
+    this.storage.size++;
+    this.storage[this.size() + this.storage.head - 1] = value;
+  }
+
+  dequeue() {
+    if (this.size() === 0) {
+      return;
+    }
+
+    let val = this.storage[this.storage.head];
+    this.storage[this.storage.head] = undefined;
+    this.storage.size--;
+    this.storage.head++;
+    return val;
+  }
+
+  size() {
+    if (this.storage.size === undefined) {
+      this.storage.size = 0;
+    }
+
+    return this.storage.size;
+  }
+
+
 
 }
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,33 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.storage.size = 0;
+  }
+
+  push(value) {
+    this.storage[this.size()] = value;
+    this.storage.size++;
+  }
+
+  pop() {
+    if (this.size() === 0) {
+      return;
+    }
+
+    let val = this.storage[this.size() - 1];
+    this.storage[this.size() - 1] = undefined;
+    this.storage.size--;
+    return val;
+  }
+
+  size() {
+    if (this.storage.size === undefined) {
+      this.storage.size = 0;
+    }
+
+    return this.storage.size;
+
   }
 
 }
