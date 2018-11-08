a43dd2911beef28e70b4b9c6d6f4d277c09b5a7e
velezjose <velezq.jose@gmail.com>
2018-11-08 10:29:40 -0800

    Implement Linked List

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,36 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    let n = Node(value);
+
+    if (list.head === null) {
+      list.head = n;
+      list.tail = n;
+      return;
+    }
+    
+    list.tail.next = n;
+    list.tail = n;    
   };
 
   list.removeHead = function() {
+    let n = list.head;
+    list.head = list.head.next;
+    n.next = null;
+    return n.value;
   };
 
   list.contains = function(target) {
+    let n = list.head;
+    
+    while (n) {
+      if (n.value === target) {
+        return true;
+      }
+      n = n.next;
+    }
+  
+    return false;
   };
 
   return list;
