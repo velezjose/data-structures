ea62113f41fac87ce760dae52b3f87f4985891b6
velezjose <velezq.jose@gmail.com>
2018-11-09 10:28:59 -0800

    Implement Set using an array

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,44 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = []; 
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  if (this.contains(item)) {
+    return;
+  }
+  
+  this._storage.push(item);
+  console.log('adding ' + item + ' :' + this._storage);
 };
 
 setPrototype.contains = function(item) {
+  for (let i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      return true;
+    }
+  }
+  return false;
 };
 
 setPrototype.remove = function(item) {
+  if (!this.contains(item)) {
+    return;
+  }
+
+  for (let i = 0; i < this._storage.length; i++) {
+    if (this._storage[i] === item) {
+      this._storage.splice(i, 1);
+    }
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  add - O(n) bcz we check if the item is contained in the Set
+  contains - O(n) 
+  remove - O(n) 
  */
