cc4f9a225f9ba9a1be5f9e66302b17142c0ea799
velezjose <velezq.jose@gmail.com>
2018-11-09 11:11:53 -0800

    Implement Hash Table (two optional missing tests)

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -7,20 +7,70 @@ var HashTable = function() {
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  let hash = [k, v];
+  let arr = this._storage.get(index);
+  
+  if (arr === undefined) {
+    this._storage.set(index, []);
+    let arr = this._storage.get(index);
+    arr.push(hash);
+    return;
+
+  } else { 
+
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i][0] === k) {
+        arr[i][1] = v;
+        return;
+      }
+    }
+
+    arr.push(hash);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  let arr = this._storage.get(index);
+
+  if (arr === undefined) {
+    return;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] === k) {
+      return arr[i][1];
+    }
+  }
 };
 
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+
+  let arr = this._storage.get(index);
+
+  if (arr === undefined) {
+    return;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i][0] === k) {
+      arr.splice(i, 1);
+    }
+  }
 };
 
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+  insert - best case O(1), worst case O(n)
+  retrieve - best case O(1), worst case O(n)
+  remove - best case O(1), worst case O(n)
+
  */
 
 
